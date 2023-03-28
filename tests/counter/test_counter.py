26a8129e9dd000a0ff2de78f31a223752723a10c
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-03-27 21:44:36 -0300

    requisito 10 realizado

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,22 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def read_file():
+    return """
+    This text is a test,
+    this text is a test,
+    this text is a test,
+    this text is a test,
+    this text not a test,
+    """
+
+
+def test_counter(read_file):
+    with patch("builtins.open", mock_open(read_data=read_file)):
+        assert count_ocurrences("filename", "this") == 5
+        assert count_ocurrences("filename", "test") == 5
+        assert count_ocurrences("filename", "is a") == 4
+        assert count_ocurrences("filename", "not a") == 1
